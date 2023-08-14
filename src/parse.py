8caeb8917c9cb0d5177a8f2fcc22a85dd8afaeeb
dart-bird <dartbird.yu@gmail.com>
2023-08-14 23:04:35 +0900

    :hammer: Refactoring code & Add enum (parse type)

--- src/parse.py
+++ src/parse.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 import chardet
 import requests
 import re
+from enum import Enum
+
+class ParseType(Enum):
+    SINGLE = 1
+    MULTI = 2
 
 class WebCrawling:
     def __init__(self, urls: List[str], tags: List[str]):
@@ -30,7 +35,7 @@ class WebCrawling:
     def _runEach(self) -> None:
         data = list()
         for url in self._urls:
-            cleaned_data = self._cleanData(self._getData(url))
+            cleaned_data = self._getData(url)
             data.append(cleaned_data)
         self._data = data
 
@@ -38,40 +43,28 @@ class WebCrawling:
         data = list()
         try:
             response = requests.get(url, headers=self._header)
-            detected_encoding = chardet.detect(response.content)['encoding']
-            if detected_encoding != 'EUC-KR':
-                response.encoding = 'utf8'
-            else:
-                response.encoding = detected_encoding
             if response.status_code != 200:
                 raise Exception('The status code is not 200')
             soup = BeautifulSoup(response.text, 'html.parser')
             title, content, date = self._tags
-            data.append(self._extractData(soup, title))
-            data.append(self._extractContent(soup, content))
-            data.append(self._extractData(soup, date))
+            data.append(self._extractData(soup, title, ParseType.SINGLE))
+            data.append(self._extractData(soup, content, ParseType.MULTI))
+            data.append(self._extractData(soup, date, ParseType.SINGLE))
             return data
         except Exception as e:
             print('Error occured while getting data\n{0}'.format(e))
             return None
 
-    def _extractData(self, soup: BeautifulSoup, tag: str) -> str:
-        return soup.select(tag)[0].text
+    def _extractData(self, soup: BeautifulSoup, tag: str, parse_type: ParseType) -> str:
+        if parse_type == ParseType.SINGLE:
+            return self._cleanText(soup.select_one(tag).text)
+        elif parse_type == ParseType.MULTI:
+            return [self._cleanText(selected_el.text) for selected_el in soup.select(tag)]
+        else:
+            return None
 
-    def _extractContent(self, soup: BeautifulSoup, tag: str) -> List[str]:
-        content = list()
-        data = soup.select(tag)
-        for text in data:
-            content.append(text.text)
-        return content
-    
-    def _cleanData(self, data: List) -> List:
-        title, contents, date = data
+    def _cleanText(self, text: str) -> str:
+        _text = text
         for regex in self.regex_formats:
-            title = re.sub(regex, '', title)
-            date = re.sub(regex, '', date)
-        for i, _content in enumerate(contents):
-            for regex in self.regex_formats:
-                _content = re.sub(regex, '', _content)
-            contents[i] = _content
-        return [title, contents, date]
+            _text = re.sub(regex, '', _text)
+        return _text
